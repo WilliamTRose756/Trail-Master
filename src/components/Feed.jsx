d4e03c4d1865965cd38fb9dd18ac6d4c0a01c97c
Bobodahobo54 <95511834+WilliamTRose756@users.noreply.github.com>
2022-07-17 18:22:48 -0500

    Posts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,23 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+
 import Post from "./Post";
 function Feed() {
   return (
-    <Box sx={{ backgroundColor: "yellow" }} p={2} flex={4}>
-      Feed
+    <Box p={2} flex={4}>
+      <Post />
       <Post />
     </Box>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,61 @@
-import { Container } from "@mui/material";
+import { Checkbox, Container } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 function Post() {
-  return <Container>Post</Container>;
+  return (
+    <Container>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "#344966" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Lost Lake Loop"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="20%"
+          image="https://images.pexels.com/photos/1578750/pexels-photo-1578750.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+            tempora quasi rerum perspiciatis libero at saepe fugit nisi incidunt
+            officia!
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Container>
+  );
 }
 
 export default Post;
